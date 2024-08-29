6b7eaade5f31a9d8555aff84e3a03ef952171d12
nathanfalvisi <142191414+nathanfalvisi@users.noreply.github.com>
2024-08-29 09:03:51 -0300

    Add files via upload

---
+++ analex.h
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define NUM 256
+
+int tokenval;
+
+int analex(){
+    char ch;
+
+    ch = getchar();
+
+    if(ch == '+')
+        return '+';
+
+    if(ch == '-')
+        return '-';
+
+    if(ch == '*')
+        return '*';
+
+    if(ch == '/')
+        return '/';
+
+    if(ch == ';')
+        return ';';
+
+    if(ch == '(')
+        return '(';
+
+    if(ch == ')')
+        return ')';
+
+    if(isdigit(ch)){
+        tokenval = 0;
+
+    while (isdigit(ch)){
+        tokenval = tokenval * 10 + (ch - '0');
+        ch = getchar();
+    }
+    ungetc(ch, stdin);
+    return NUM;
+    }
+
+    if (!isspace(ch)){
+        printf("Erro Lexico: Caracter Inserido Invalido! '%c'", ch);
+        exit(1);
+    }
+
+    return analex();
+}
---
+++ sint.c
@@ -0,0 +1,111 @@
+#include "analex.h"
+#include "pilha.h"
+#define MAX 1000
+
+int token;
+
+void E();
+void E_linha();
+void T();
+void T_linha();
+void F();
+
+
+void erro_sint(){
+    printf("ERRO SINTATICO!!");
+    exit(1);
+}
+
+void consome(int t){
+    if (token == t)
+        token = analex();
+    else
+        erro_sint();
+}
+
+void E(){
+    T();
+    E_linha();
+}
+
+void E_linha(){
+    switch(token){
+        case '+':
+            consome('+');
+            T();
+            a = pop();
+            b = pop();
+            push(a + b);
+            E_linha();
+            break;
+        case '-':
+            consome('-');
+            T();
+            a = pop();
+            b = pop();
+            E_linha();
+            break;
+    }
+}
+
+void T(){
+    F();
+    T_linha();
+}
+
+void T_linha(){
+    switch(token){
+        case '*':
+            consome('*');
+            F();
+            a = pop();
+            b = pop();
+            T_linha();
+            break;
+        case '/':
+            consome('/');
+            F();
+            a = pop();
+            b = pop();
+            T_linha();
+            break;
+    }
+}
+
+void F(){
+    switch(token){
+        case '(':
+            consome('(');
+            E();
+            consome(')');
+            break;
+        case NUM:
+            consome(NUM);
+            break;
+        default:
+            erro_sint();
+    }
+}
+
+int main(){
+    token = analex();
+    E();
+    if(token == ';')
+        printf("NÃO TEM ERROS SINTATICOS!!");
+    else
+        erro_sint();
+    return 0;
+}
+
+/*int main(){
+    printf("Digite uma sequencia de caracteres:\n");
+    int token;
+    while ((token = analex()) != EOF) {
+        if (token == NUM)
+            printf("Numero lido: %d\n", tokenval);
+        else
+            printf("Operador ou simbolo lido: '%c'\n", token);
+    }
+ }*/
+
+
